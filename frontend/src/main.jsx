15c9211f3942f206c86682296c18f31a85af138a
Yared Mengiste <yaredmengiste01@gmail.com>
2026-02-20 10:13:35 +0300

    form folder structure and add user context and apiClinent

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
+import { UserProvider } from './context/UserContext.jsx';
 import App from './App.jsx'
 const clientid = import.meta.env.VITE_APP_GOOGLE_CLIENT_ID
 createRoot(document.getElementById('root')).render(
   <GoogleOAuthProvider clientId={clientid}>
-    <App />
+    <UserProvider>
+      <App />
+    </UserProvider>
   </GoogleOAuthProvider>,
 )
