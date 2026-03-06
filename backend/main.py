bc3e44fffaf9ffcfddd24c634072089f86105835
Robel <robelnigusse123@gmail.com>
2026-03-06 19:41:08 +0300

    update CORS

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ base.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173","https://sharepdf.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
